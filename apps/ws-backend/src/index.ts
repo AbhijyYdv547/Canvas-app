d29314210ea7888ab24e6495712f4a39f3b3dab5
Abhijay Yadav <yadavabhijay@gmail.com>
2025-04-04 14:45:54 +0530

    Initial Folder Structure

---
+++ apps/ws-backend/src/index.ts
@@ -0,0 +1,10 @@
+import { WebSocketServer } from "ws";
+
+const wss = new WebSocketServer({ port: 8080 });
+wss.on("connection", function connection(ws) {
+    console.log("hello");
+    ws.on("message", () => {
+        ws.send("ping")
+    })
+
+})
