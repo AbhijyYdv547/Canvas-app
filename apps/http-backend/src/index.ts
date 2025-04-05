a11237090969c8fa5efb7db6cdb8b5e5dc9fd86b
Abhijay Yadav <yadavabhijay@gmail.com>
2025-04-05 11:27:47 +0530

    added jwt to both http-backend and ws-backend

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -1,8 +1,29 @@
 import express from "express";
+import jwt from "jsonwebtoken"
+import { JWT_SECRET } from "./config";
+import { middleware } from "./middleware";
 const app = express();
 
-app.use("/",(req,res)=>{
-    res.send("hello")
+app.post("/signup",(req,res)=>{
+    const {username,email,password} = req.body;
+
 })
 
-app.listen(3000);
+app.post("/signin",(req,res)=>{
+    const userId = 1;
+    const token = jwt.sign({
+        userId
+    },JWT_SECRET)
+
+    res.json({
+        token
+    })
+})
+
+app.post("/room",middleware,(req,res)=>{
+    res.json({
+        room: 232
+    })
+})
+
+app.listen(3001);
